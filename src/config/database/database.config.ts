865b2f1c5b135248ff9303cc4ad5aca9ab5b3c3f
Sardor-M <sardor0968@g.seoultech.ac.kr>
2025-02-24 13:28:38 +0900

    fix: db datasource configs for prod fixed

--- src/config/database/database.config.ts
+++ src/config/database/database.config.ts
@@ -2,17 +2,34 @@ import { BlogPost } from '@/modules/blog/entities/blog-post.entity';
 import { registerAs } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
+export default registerAs('database', (): TypeOrmModuleOptions => {
+  if (process.env.DATABASE_URL) {
+    // production db configs setup
+    return {
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false, 
+      },
+      entities: [BlogPost],
+      synchronize: false,
+      logging: false,
+      migrations: ['dist/migrations/*.js'],
+      migrationsRun: true,
+    };
+  }
 
-export default registerAs("database", (): TypeOrmModuleOptions => ({
-    type: "postgres",
-    host: process.env.DB_HOST || "localhost",
+  return {
+    type: 'postgres',
+    host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT || '5432', 10),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE || "shaxsiy_blog_db",
+    database: process.env.DB_DATABASE || 'shaxsiy_blog_db',
     entities: [BlogPost],
-    synchronize: process.env.NODE_ENV !== 'production', // productionda careful bolish kerak 
+    synchronize: process.env.NODE_ENV !== 'production', // productionda careful bolish kerak
     logging: process.env.NODE_ENV === 'development',
     migrations: ['dist/migrations/*.js'],
-    migrationsRun: process.env.NODE_ENV === 'production', 
-}))
+    migrationsRun: process.env.NODE_ENV === 'production',
+  };
+});
